903112793ad54c82edbf9a8a2a13e470e20fd8be
qizongjiang <qizongjiang@dinghantech.com>
2024-03-07 16:29:24 +0800

    添加日志功能

---
+++ src/Core/AppContainer/Logs/LogHelper.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Logging;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetX.AppContainer.Logs
+{
+    public class LogHelper
+    {
+        public static void RegisterLog()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(AppConst.APP_CONFIG_LOG_FILE, optional: true, reloadOnChange: true)
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+            .CreateLogger();
+            //设置Avalonia使用SerilogLogSink
+            Logger.Sink = new SerilogLogSink();
+        }
+
+        public static void CloseAndFlush()
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ src/Core/AppContainer/Logs/SerilogLogSink.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetX.AppContainer.Logs
+{
+    internal class SerilogLogSink : ILogSink
+    {
+        public SerilogLogSink()
+        {
+        }
+
+        public bool IsEnabled(LogEventLevel level, string area) => Serilog.Log.IsEnabled(ToSerilogLevel(level));
+
+        public void Log(LogEventLevel level, string area, object? source, string messageTemplate) => Log(level,area,source,messageTemplate,null);
+
+        public void Log(LogEventLevel level, string area, object? source, string messageTemplate, params object?[] propertyValues)
+        {
+            Serilog.Log.Logger.Write(ToSerilogLevel(level), messageTemplate, propertyValues);
+        }
+
+        private Serilog.Events.LogEventLevel ToSerilogLevel(LogEventLevel level)
+        {
+            return level switch
+            {
+                LogEventLevel.Verbose => Serilog.Events.LogEventLevel.Verbose,
+                LogEventLevel.Debug => Serilog.Events.LogEventLevel.Debug,
+                LogEventLevel.Information => Serilog.Events.LogEventLevel.Information,
+                LogEventLevel.Warning => Serilog.Events.LogEventLevel.Warning,
+                LogEventLevel.Error => Serilog.Events.LogEventLevel.Error,
+                LogEventLevel.Fatal => Serilog.Events.LogEventLevel.Fatal,
+                _ => Serilog.Events.LogEventLevel.Information,
+            };
+        }
+    }
+}
