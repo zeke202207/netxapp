32b0eb7021350ffc9ef38bc8ceb19f7582c1c6eb
qizongjiang <qizongjiang@dinghantech.com>
2024-03-21 13:18:06 +0800

    更新ui框架

---
+++ src/Apps/Core/AppCore/ViewModels/WorkbenchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using NetX.AppCore.Contract;
+using NetX.AppCore.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetX.AppCore.ViewModels
+{
+    [SortIndex(MainViewModel.Order)]
+    [ViewModel(ServiceLifetime.Singleton)]
+    public class WorkbenchViewModel : StartupWindowViewModel
+    {
+        public const int Order = int.MaxValue -1;
+
+        public WorkbenchViewModel(IControlCreator controlCreator, Type pageView, int order) 
+            : base(controlCreator, typeof(WorkbenchWindow), order)
+        {
+
+        }
+
+        public override Control CreateView(IControlCreator controlCreator, Type pageView) => controlCreator.CreateControl(pageView);
+    }
+}
---
+++ src/Apps/Core/AppCore/Views/WorkbenchWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using SukiUI.Controls;
+
+namespace NetX.AppCore.Views
+{
+    public partial class WorkbenchWindow : SukiWindow
+    {
+        public WorkbenchWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
