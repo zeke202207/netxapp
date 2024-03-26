d25c9329f55e7217d17496ca8b713dae821bbc7f
qizongjiang <qizongjiang@dinghantech.com>
2024-03-26 17:34:10 +0800

    美化登录页

--- src/Apps/Simple/Desktop/MyDemo/Views/StartupWindows/SplashScreenWindow.axaml.cs
+++ src/Apps/Simple/Desktop/MyDemo/Views/StartupWindows/SplashScreenWindow.axaml.cs
@@ -5,11 +5,12 @@ using FluentAvalonia.UI.Windowing;
 
 namespace MyDemo.Views
 {
-    public partial class SplashScreenWindow : AppWindow
+    public partial class SplashScreenWindow : Window
     {
         public SplashScreenWindow()
         {
-            AvaloniaXamlLoader.Load(this);
+            AvaloniaXamlLoader.Load(this); 
+
 #if DEBUG
             this.AttachDevTools();
 #endif
